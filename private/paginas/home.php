191644b21dcb97d000522a5b2c2b1d298b0d538f
drqueijo <silveriomat1234@gmail.com>
2021-06-04 00:58:51 -0300

    base de codigo para admin

---
+++ private/paginas/home.php
@@ -0,0 +1,52 @@
+<?php
+	//verificar se a variável $pagina não existe
+	if ( !isset ( $pagina ) ) exit;
+
+?>
+<h1>Produtos em Destaque:</h1>
+<div class="row">
+	<?php
+		//selecionar 6 produtos - rand -> sorteio - limit limitar o nr de resultado
+		$sql = "select * from produto order by rand() limit 6";
+		//executar o sql
+		$result = mysqli_query($con, $sql);
+
+		//separar os dados por linha
+		while ( $dados = mysqli_fetch_array( $result ) ) {
+			//separar
+			$id = $dados["id"];
+			$produto = $dados["produto"];
+			$imagem = $dados["imagem"];
+			$valor = $dados["valor"];
+			$promo = $dados["promo"];
+
+			//se tiver promo - valor = valor da promo
+			//senao valor = valor do produto
+
+			//se a promo esta vazio - valor = valor do produto
+			if ( empty ( $promo ) ) {
+				//1499.99 -> 1.499,99
+				$valor = "R$ " . number_format($valor, 2, ",", ".");
+				$de = "";
+			} else {
+				//valor normal
+				$de = "R$ " . number_format($valor, 2, ",", ".");
+				//valor promocional
+				$valor = "R$ " . number_format($promo, 2, ",", ".");
+			}
+
+			echo "<div class='col-12 col-md-4 text-center'>
+				<img src='produtos/{$imagem}p.jpg' alt='{$produto}' class='w-100'>
+				<h2>{$produto}</h2>
+				<p class='de'>{$de}</p>
+				<p class='valor'>{$valor}</p>
+				<p>
+					<a href='index.php?pagina=produto&id={$id}' class='btn btn-success btn-lg w-100'>
+					Detalhes
+					</a>
+				</p>
+			</div>";
+
+		}
+	?>
+</div>
